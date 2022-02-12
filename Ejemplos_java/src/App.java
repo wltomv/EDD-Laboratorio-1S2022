7239084c89169897518e4045c10fe50be99eb7b7
Walter <owaldovelasz@gmail.com>
2022-02-12 10:08:52 -0600

    Modificación de la ubicación del archivo de ordenamientos.

--- Ejemplos_java/src/App.java
+++ Ejemplos_java/src/App.java
@@ -1,6 +1,7 @@
 
 import Listas.ListaCircularSimple;
 import Listas.ListaDoble;
+import Arreglos.Ordenamientos;
 
 public class App {
     public static void main(String[] args) throws Exception {       
@@ -8,27 +9,26 @@ public class App {
         ListaDoble myList= new ListaDoble();
 
         int[] arreglo={4,7,42,1,12};
-        // myList.insertarAlFinal(1);
-        // myList.insertarAlFinal(2);
-        // myList.insertarAlFinal(3);
-        // myList.insertarAlFinal(4);
-        // myList.insertarAlFinal(5);
-        // myList.insertarAlFinal(6);
-        // myList.visualizar();
+        myList.insertarAlFinal(1);
+        myList.insertarAlFinal(2);
+        myList.insertarAlFinal(3);
+        myList.insertarAlFinal(4);
+        myList.insertarAlFinal(5);
+        myList.insertarAlFinal(6);
+        myList.visualizar();
 
         // //PRUEBA DE LISTA CIRCULAR
-        // ListaCircularSimple circular= new ListaCircularSimple();
-        // circular.insertar(1);
-        // circular.insertar(2);
-        // circular.insertar(3);
-        // circular.insertar(4);
-        // circular.imprimir();
+        ListaCircularSimple circular= new ListaCircularSimple();
+        circular.insertar(1);
+        circular.insertar(2);
+        circular.insertar(3);
+        circular.insertar(4);
+        circular.imprimir();
 
         Ordenamientos.quicksort(arreglo);
         // Ordenamientos.insercion(arreglo);
         // Ordenamientos.seleccion(arreglo);
         // Ordenamientos.burbuja(arreglo);
-        
         Ordenamientos.imprimir(arreglo);
         
     }
--- Ejemplos_java/src/Arreglos/Ordenamientos.java
+++ Ejemplos_java/src/Arreglos/Ordenamientos.java
@@ -1,3 +1,5 @@
+package Arreglos;
+
 public class Ordenamientos {
 
 
